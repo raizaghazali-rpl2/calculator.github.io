60c81d25c84fda0b34f1e13432b8c467e1a8990f
raizaghazali-rpl2 <140592157+raizaghazali-rpl2@users.noreply.github.com>
2023-09-14 08:52:41 +0700

    Add files via upload

---
+++ javva.js
@@ -0,0 +1,24 @@
+var panjang = document.querySelector(".panjang");
+var lebar = document.querySelector(".lebar");
+var calculateButton = document.querySelector(".luas");
+var BMI,  height, weight;
+
+function myFunction(){
+    height = panjang.value;
+    weight = lebar.value;
+    BMI = weight*height/2;
+    alert("BMI luas segitiga adalah :" + BMI);
+}
+// luas segitiga
+var panjangg = document.querySelector(".panjangg");
+var lebarr = document.querySelector(".lebarr");
+var calculateButton = document.querySelector(".luasP");
+var BMI,  height, weight;
+
+function myFunctionn(){
+    height = panjangg.value;
+    weight = lebarr.value;
+    BMI = weight*height;
+    alert("BMI luas Persegi adalah :" + BMI);
+}
+// luas persegi
